be65f03168fc6c1f7f9c7b87193145c6ddad1e83
Gatu-1548 <jorge.alfredo.mamani@gmail.com>
2025-04-28 09:47:07 -0400

    cambios

--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -1,14 +1,44 @@
 import { Navigate } from 'react-router-dom';
+import  jwt_decode  from 'jwt-decode';
 
-const PrivateRoute = ({ children }) => {
+const PrivateRoute = ({ children,allowedRoles }) => {
   const token = localStorage.getItem('token');
 
-  // Si no hay token, redirige al login
   if (!token) {
     return <Navigate to="/login" replace />;
   }
 
-  return children; // Si hay token, muestra la ruta protegida
+  try {
+    // Decodificar el token
+    const decoded = jwt_decode(token);
+    console.log(decoded);
+    //Opcional: Verificar si el rol está permitido
+    if (allowedRoles && !allowedRoles.includes(decoded.role)) {
+      // Si el rol del usuario no está en los roles permitidos, redirigir
+      return <Navigate to="/dashboard" replace />;
+    }
+
+    return children; // Todo bien, muestra la ruta protegida
+  } catch (error) {
+    console.error('Error al decodificar el token', error);
+    return <Navigate to="/login" replace />;
+  }
 };
+
+
+// import { Navigate } from 'react-router-dom';
+
+// const PrivateRoute = ({ children }) => {
+//   const token = localStorage.getItem('token');
+
+//   // Si no hay token, redirige al login
+//   if (!token) {
+//     return <Navigate to="/login" replace />;
+//   }
+
+//   return children; // Si hay token, muestra la ruta protegida
+// };
+
+// export default PrivateRoute;
 
 export default PrivateRoute;
