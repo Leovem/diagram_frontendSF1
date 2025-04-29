ef2e01845c5030f6167b86544e76ae035872887e
Gatu-1548 <jorge.alfredo.mamani@gmail.com>
2025-04-29 19:37:47 -0400

    cambios en socket

--- src/figma/socketService.js
+++ src/figma/socketService.js
@@ -2,7 +2,7 @@
 import { io } from 'socket.io-client';
 
 // Creamos la conexión al backend
-export const socket = io('http://localhost:5000');
+export const socket = io('https://backenddesing-production.up.railway.app');
 // ,{
 //   transports: ['websocket'], // Usa WebSocket directamente
 //   withCredentials: true,     // Permite cookies si se requieren
